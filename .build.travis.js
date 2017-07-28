2bc85a3be76e4b8816add0895a3bd3ca564c9464
Shengming Yuan <syua9708@flhs.us>
2017-07-28 12:45:15 +0800

    build

--- .build.travis.js
+++ .build.travis.js
@@ -1,15 +1,1 @@
-var sys = require('sys')
-var exec = require('child_process').exec;
-var child;
-child = exec("pwd", function(error, stdout, stderr) {
-    sys.print('stdout: ' + stdout);
-    sys.print('stderr: ' + stderr);
-    if (error !== null) {
-        console.log('exec error: ' + error);
-    }
-});
-var sys = require('sys')
-var exec = require('child_process').exec;
-
-function puts(error, stdout, stderr) { sys.puts(stdout) }
-exec("wget -qO- http://ipecho.net/plain ; echo", puts);
+console.log('Build： OK')
